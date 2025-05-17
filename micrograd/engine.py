be388b6fedc0cf586a81228dfb20b66c30b87f4d
achhibbar <ashchhibbar@gmail.com>
2025-05-16 20:33:58 -0400

    makemore

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,94 +1,128 @@
-
 class Value:
     """ stores a single scalar value and its gradient """
 
+    # Explanation of how backward() and _backward works:
+    # the _backward() function is defined inside __add__() and assigned to out._backward, the variables self.grad, 
+    # other.grad, and out.grad refer to the specific Value objects involved in that particular addition operation. 
+    # This behavior is due to Python's closure mechanism, which captures the surrounding scope's variables by reference.
+
+
+
+    #__init__ is used when a val is initialized either by making a new Value or by using an op like __pow__
+    #if a Value is created by an op, the values used in the op are store in children and the op stored in op
+    #data = value of the object
+    #childred = the values that were used in an op to create this value (e.g. __mul__)
+    #_op = if an op (e.g. __add__) was to use to create this val, it's string to identify the op is store in _op
     def __init__(self, data, _children=(), _op=''):
-        self.data = data
-        self.grad = 0
+        self.data = data  # the actual scalar value
+        self.grad = 0  # the gradient of this value
         # internal variables used for autograd graph construction
-        self._backward = lambda: None
-        self._prev = set(_children)
-        self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self._backward = lambda: None  # This will recalculate the gradients of the children's Values using the gradient of this Value
+        self._prev = set(_children)  # tuple of children nodes. A set is used to avoid duplicates
+        self._op = _op  # the operation that produced this node stored as a string, for graphviz / debugging / etc
 
+    #using double underscores overloads the addition (+) operation in python, so + operations used by Value objects will be redirected here
     def __add__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data, (self, other), '+')
+        #check if other is a Value object, if not then make it a Value. 
+        #This allows us to add/subtract etc. without needed to make a Value object each time e.g. Value(4) + 5
+        other = other if isinstance(other, Value) else Value(other)  # ensure other is a Value
+        out = Value(self.data + other.data, (self, other), '+')  # create new Value for the sum
 
+        #the underscore before backward indicates this is a private function not meant for API usage unlike the backward function defined later
+        #define what happens in backprop for addition using the chain rule
+        #this will be called when _backward is called on the output value, typically the loss function
+        #the children's gradient is calculated by the their parent's (out) gradient using the chain rule
         def _backward():
-            self.grad += out.grad
-            other.grad += out.grad
-        out._backward = _backward
+            self.grad += out.grad  # using closer, self.grad is the gradient of the Value object that was used in the op to create out
+            other.grad += out.grad  # propagate gradient to other
+
+        #assigns the above _backward function to out._backward and is called during back prop to calculate out's children's grads.
+        out._backward = _backward  # when _backward is called on the Value out, it will recalculate the gradients in self and other using out's gradient
 
         return out
 
     def __mul__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data, (self, other), '*')
+        other = other if isinstance(other, Value) else Value(other)  # ensure other is a Value
+        out = Value(self.data * other.data, (self, other), '*')  # create new Value for the product
 
         def _backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
-        out._backward = _backward
+            self.grad += other.data * out.grad  # propagate gradient to self
+            other.grad += self.data * out.grad  # propagate gradient to other
+        out._backward = _backward  # set the backward function
 
         return out
 
     def __pow__(self, other):
-        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
-        out = Value(self.data**other, (self,), f'**{other}')
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"  # ensure other is int/float
+        out = Value(self.data**other, (self,), f'**{other}')  # create new Value for the power
 
         def _backward():
-            self.grad += (other * self.data**(other-1)) * out.grad
-        out._backward = _backward
+            self.grad += (other * self.data**(other-1)) * out.grad  # propagate gradient to self
+        out._backward = _backward  # set the backward function
 
         return out
 
     def relu(self):
-        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')  # if self.data < 0, set to 0
 
         def _backward():
-            self.grad += (out.data > 0) * out.grad
-        out._backward = _backward
+            self.grad += (out.data > 0) * out.grad  #if out.data is greater than 0, * by out.grad and add to self.grad
+        out._backward = _backward  # set the backward function
 
         return out
 
     def backward(self):
-
+        # backward is called on a single scalar output that is typically the loss loss
         # topological order all of the children in the graph
-        topo = []
-        visited = set()
-        def build_topo(v):
-            if v not in visited:
-                visited.add(v)
-                for child in v._prev:
-                    build_topo(child)
-                topo.append(v)
-        build_topo(self)
+        topo = [] #empty list that will store the nodes
+        visited = set() #will be populated by nodes as they are visited during back prop to avoid redundant vists. Set is used to avoid duplicates
+        #the function within backward that builds the topological graph. It's used recursively to build the graph
+        def build_topo(v): #depth 1st traversal
+            if v not in visited: #check if the node has been visited
+                visited.add(v) #if it hasn't been visitied, add it to the visited list
+                for child in v._prev: #establish a for loop that goes through the children of the node (the values used in an op to create it)
+                    build_topo(child) #recursively build the topo of v's child (which will then build the topo of v's child's, child recursively)
+                topo.append(v) #append the node/value to topo, since this is done after the recursive traversal, it start with the deepest depth (closest to the input)
+        build_topo(self) #the 1st call to building the topographical graph within back prop
 
         # go one variable at a time and apply the chain rule to get its gradient
-        self.grad = 1
-        for v in reversed(topo):
-            v._backward()
+        self.grad = 1 #as is standard, set the gradient of the final output value to 1
+        # since the topo list is depth 1st (start with values closts to input and end with loss), 
+        # the order is reversed to start with the node/Value that is typically the loss
+        for v in reversed(topo): 
+            v._backward() #call the back prop functions store in the loss Value 1st and go backward from there using a reversed topo list. Back prop is done using the chain rule to calculate the gradient of the children nodes
 
+    # the following methods cleverly reuse the __add__ and __mul__ methods to perform new arithmetic operations
+
+    #get the negative of a Value by making use of the existing __mul__ method
     def __neg__(self): # -self
         return self * -1
 
+    #since this is called on the right side value (e.g, 5 + Value), self is the Value and other is the number value
     def __radd__(self, other): # other + self
         return self + other
 
+    #a simple way of defining subtraction by using the existing __add__ method
     def __sub__(self, other): # self - other
         return self + (-other)
 
     def __rsub__(self, other): # other - self
         return other + (-self)
 
+    #remember that this is only called when a Value is on the right, e.g. 5 * x, and therefore rmul is called on the x which is store in self
     def __rmul__(self, other): # other * self
         return self * other
 
+    #a simple way to divide using the existing __mul__ and __pow__ methods
     def __truediv__(self, other): # self / other
         return self * other**-1
 
+    # a simple way to divide using the existing __mul__ and __pow__ methods when the Value is on the right
     def __rtruediv__(self, other): # other / self
         return other * self**-1
 
+    # repr is used to return a string representation of the Value object. This can be accessed by using print(Value)
     def __repr__(self):
-        return f"Value(data={self.data}, grad={self.grad})"
+        #return f"Value(data={self.data}, grad={self.grad})"  # string representation of Value
+        # return the Value object, the gradient of the Value object, the operation used to create the Value object, and the children of the Value object as a string
+        return f"Value(data={self.data}, grad={self.grad}, op={self._op}, prev={len(self._prev)})"
--- micrograd/nn.py
+++ micrograd/nn.py
@@ -3,6 +3,8 @@ from micrograd.engine import Value
 
 class Module:
 
+    # zero the gradients of all the parameters. This can be called on a Neuron or Layer object, 
+    # but is more useful for the MLP object (which contains layers of neurons)
     def zero_grad(self):
         for p in self.parameters():
             p.grad = 0
@@ -11,29 +13,51 @@ class Module:
         return []
 
 class Neuron(Module):
-
+    #nin = no of inputs to the neuron, nonlin = whether a non linearity is applied (set to True by default)
+    #self allows the class to store its attributes for use later. Without self, the method would be static
     def __init__(self, nin, nonlin=True):
+        #create a weight for every input using Value class from the engine, init with a random val between -1 and 1
+        #inputs can be the inputs to a MLP or the outputs of neurons from previous layer in the MLP
+        # since weights are created without any mathematical operations, they don't have any children
         self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        #create a single bias value for the neuron using Value class
         self.b = Value(0)
+        #store whether the neuron has a non-linearity applied to it
         self.nonlin = nonlin
 
+    #call the neuron using () and send as args the inputs
+    #this is where the relationships between Values is created, as Values are mulitplied created parents and children
     def __call__(self, x):
+        #zip together the weights (w) and inputs (x) to create a list of tuples
+        #multiply each tuple wi, xi with each other, then sum them and add the bias value
+        # in practice the bias is added to the 1st element of the sum, and then the sum is added to the 2nd element of the sum and so on
+        # that's why when visualizing the graph using graphviz, the bias is added to the 1st (input * weight) and then the sum is added to the 2nd (input * weight) and so on
         act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+        #apply a nonlinearity relu to the summed output + bias if the neuron has one
         return act.relu() if self.nonlin else act
 
+    #return the list of weights and the bias for the neuron
     def parameters(self):
         return self.w + [self.b]
 
+    #if the name of the neuron is typed and entered, this returns whether it uses ReLU or is linear (repr = representation overloaded)
     def __repr__(self):
         return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
 class Layer(Module):
-
+    #create a layer of Neurons, each with no. of inputs in nin and the no. of neurons in nout
+    #**kwargs is used to indicate that the function can accept any number of keyword arguments
     def __init__(self, nin, nout, **kwargs):
+        #create the neurons and store the list of them in self.neurons
+        #neurons each have no. of weights in nin, and no. of neurons created are nout
+        #**kwargs can pass whether the neurons should have a nonlin or not
         self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
 
+    # x are a set of inputs, either the MLP's inputs or the outputs from the preceding layer
     def __call__(self, x):
+        #out is a list of outputs of all the neurons in the layer when input x is passed to them as an input
         out = [n(x) for n in self.neurons]
+        #return a scaler value if there is only 1 neuron in the layer
         return out[0] if len(out) == 1 else out
 
     def parameters(self):
@@ -44,12 +68,28 @@ class Layer(Module):
 
 class MLP(Module):
 
+    #nin stores the no. of inputs to the MLP and nouts is a list that defines the no. of layers and no. of neurons in each layer
+    #MLP(3, [4, 4, 1]) = 3 inputs, 2 hidden layers and 1 neuron in the ouput layer. Each hidden layer contains 4 neurons
     def __init__(self, nin, nouts):
-        sz = [nin] + nouts
+        sz = [nin] + nouts #prepend the inputs to the MLP layers so we'll know how many weights the 1st hidden layer should have per neuron
+        #create the no. of layers as there are elements in the nouts list (this doesn't contain the input layer)
+        #each layers no. of neuron's inputs should be == the previous layer's no/ of neurons
+        #1st hidden layer of MLP will have no. of weights per neuron == the no. of MLP inputs and no of neurons specified in nouts
+        #since the MLP inputs aren't created here, the 1st layer created is the 1st hidden layer which needs to have weights == to no. of inputs
+        #e.g. 1st hiddent layer created has inputs of sz[i] which is the no. of inputs of the MLP (3) and no of neurons = sz[i+1] which is 4 in this case
+        #since sz prepended the no of inputs to the MLP architecture
+        #nonlin=i!=len(nouts)-1 ensure each layer's neurons has a nonlinearity apart from the last layer (ouput layer)
+        #if i is not == to the last layer, set nonlin to true, otherwise set to false
         self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
 
+    #do forward pass using the inputs passed in x
+    #starting with the 1st hidden layer and finishing with the final output layer
     def __call__(self, x):
         for layer in self.layers:
+            #at the beginning of the 1st iteration, x is the MLP's inputs
+            #at the end of the iteration x is overwritten with the current layers output
+            #the current layer's output is then fed into the next layer
+            #this extablishes the relationships of the neurons between layers which are used for back prop
             x = layer(x)
         return x
 
